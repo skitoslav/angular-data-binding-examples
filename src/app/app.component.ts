9f644504f02571935aa7a62f7e074c90520dfd24
Svyatoslav Gerasimov <s.gerasimov.work@gmail.com>
2024-11-21 20:31:39 +0300

    fixed an import in styles.scss related to angular-v19; replaced ngFor in Navigation component with @for

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @Component({
-    selector: 'adbe-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
-    standalone: false
+  selector: 'adbe-root',
+  imports: [NavigationComponent],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  standalone: false,
 })
 export class AppComponent {
   title = 'angular-data-binding-examples';
--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,12 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 
 import { ApplicationRouteEnum } from '../../libs/shared/enums/navigation/application';
 
 @Component({
     selector: 'adbe-navigation',
-    imports: [CommonModule, RouterLink],
+    imports: [RouterLink],
     templateUrl: './navigation.component.html',
     styleUrls: ['./navigation.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush
